ee7166ec440d6cb1bb9752c41621f1dcfacadb71
Blake Watters <blake@opsani.com>
2020-08-27 21:30:49 -0700

    Add new endpoints for resource consumption

--- README.md
+++ README.md
@@ -6,10 +6,16 @@ and predictable performance.
 
 Built with [Fiber](https://docs.gofiber.io/) and [FastHTTP](https://github.com/valyala/fasthttp).
 
-The app exposes two endpoints:
+The app exposes several endpoints:
 
 * `/` - Returns a 200 (Ok) `text/plain` respomse.
 * `/metrics` - Metrics in Prometheus format for scraping.
+* `/cpu{?duration}` - Consume CPU resources for the given duration (in Golang Duration string format). Default: `100ms`
+* `/memory{?size}` - Consume memory resources by allocating a byte array of the given size (in human readable byte string format). Default: `10MB`
+* `/time{?duration}` - Consume time by sleeping for the given duration (in Golang Duration string format). Default: `100ms`
+
+The resource endpoints of `cpu`, `memory`, and `time` are useful for triggering the artificial consumption
+of resources for testing autoscale behaviors, error handling, response to latency, etc.
 
 ## Docker images
 
--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"fmt"
+	"runtime"
+	"time"
+
 	"github.com/ansrivas/fiberprometheus"
 	"github.com/gofiber/fiber"
 	"github.com/gofiber/fiber/middleware"
+	"github.com/inhies/go-bytesize"
+
+	_ "go.uber.org/automaxprocs"
 )
 
 func main() {
@@ -19,5 +26,61 @@ func main() {
 		c.Send("move along, nothing to see here")
 	})
 
+	app.Get("/cpu", func(c *fiber.Ctx) {
+		duration, err := time.ParseDuration(c.Query("duration", "100ms"))
+		if err != nil {
+			c.Next(err)
+			return
+		}
+
+		consumeCPU(duration)
+		c.Send(fmt.Sprintf("consumed CPU for %v\n", duration.String()))
+	})
+
+	app.Get("/memory", func(c *fiber.Ctx) {
+		size, err := bytesize.Parse(c.Query("size", "10MB"))
+		if err != nil {
+			c.Next(err)
+			return
+		}
+
+		data := make([]byte, size)
+		c.Send(fmt.Sprintf("allocated %v (%d bytes) of memory\n", size.String(), len(data)))
+	})
+
+	app.Get("/time", func(c *fiber.Ctx) {
+		duration, err := time.ParseDuration(c.Query("duration", "100ms"))
+		if err != nil {
+			c.Next(err)
+			return
+		}
+
+		time.Sleep(duration)
+		c.Send(fmt.Sprintf("slept for %v\n", duration.String()))
+	})
+
 	app.Listen(8080)
+}
+
+func consumeCPU(duration time.Duration) {
+	maxProcs := runtime.GOMAXPROCS(0)
+	stop := make(chan bool)
+
+	for i := 0; i < maxProcs; i++ {
+		go func() {
+			for {
+				select {
+				case <-stop:
+					return
+				default:
+				}
+			}
+		}()
+	}
+
+	time.Sleep(duration)
+
+	for i := 0; i < maxProcs; i++ {
+		stop <- true
+	}
 }
