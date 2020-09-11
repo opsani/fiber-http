fdc62f4f5f8800ecdfb072d274c1d2984341e1b7
Blake Watters <blake@opsani.com>
2020-09-11 06:00:11 -0700

    Test against localhost for lower latency

--- main_test.go
+++ main_test.go
@@ -80,7 +80,7 @@ func TestTime(t *testing.T) {
 
 func TestRequest(t *testing.T) {
 	app := newApp()
-	req := httptest.NewRequest("GET", "http://localhost:8080/request?url=http://opsani.com/", nil)
+	req := httptest.NewRequest("GET", "http://localhost:8080/request?url=http://localhost:8080/", nil)
 
 	resp, err := app.Test(req)
 	if err != nil {
